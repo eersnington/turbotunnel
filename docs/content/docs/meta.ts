2557ac26890969475e3b9b9f6782c7a7b0e54f24
Sree Narayanan <sreeaadhi07@gmail.com>
2026-07-16 20:48:31 +0400

    docs: organize CLI reference

--- docs/content/docs/meta.ts
+++ docs/content/docs/meta.ts
@@ -2,5 +2,5 @@ import { defineMeta } from "blume";
 
 export default defineMeta({
   title: "Docs",
-  pages: ["index", "installation", "deploy", "http", "configuration", "how-it-works", "limits"],
+  pages: ["index", "installation", "commands", "learn", "help"],
 });
